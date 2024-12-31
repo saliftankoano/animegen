350ac823ad541a23368adb0e64a5832a504f2dd2
Salif Tankoano <tanksalif@gmail.com>
2024-12-30 19:09:53 -0500

    fix join btn color schema for CTA

--- components/JoinButton.tsx
+++ components/JoinButton.tsx
@@ -1,13 +1,17 @@
 import { SignUpButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
-export function JoinButton() {
+export function JoinButton({ color }: { color?: string }) {
   return (
     <>
       <Button
         asChild
         size="lg"
-        className="bg-primary text-primary-foreground hover:transition-all hover:duration-300 hover:bg-white hover:text-black"
+        className={`bg-primary text-primary-foreground hover:transition-all hover:duration-300 hover:cursor-pointer ${
+          color === "white"
+            ? "bg-white text-purple-700 hover:bg-purple-100"
+            : "hover:bg-white hover:text-black"
+        }`}
       >
         <SignUpButton
           signInForceRedirectUrl={"/feed"}
--- components/create-image-cta.tsx
+++ components/create-image-cta.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { useUser } from "@clerk/nextjs";
+import { JoinButton } from "./JoinButton";
 export function CreateImageCTA() {
+  const { user } = useUser();
+
   return (
     <Card className="relative overflow-hidden mt-8">
       <div className="absolute inset-0 bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 animate-gradient"></div>
@@ -12,15 +16,19 @@ export function CreateImageCTA() {
         <p className="text-lg mb-4">
           Join the fun and let your imagination run wild!
         </p>
-        <Link href="/create">
-          <Button
-            size="lg"
-            variant="secondary"
-            className="font-semibold bg-white text-purple-700 hover:bg-purple-100"
-          >
-            Let&apos;s Go!
-          </Button>
-        </Link>
+        {user ? (
+          <Link href="/create">
+            <Button
+              size="lg"
+              variant="secondary"
+              className="font-semibold bg-white text-purple-700 hover:bg-purple-100"
+            >
+              Let&apos;s Go!
+            </Button>
+          </Link>
+        ) : (
+          <JoinButton color="white" />
+        )}
       </CardContent>
     </Card>
   );
