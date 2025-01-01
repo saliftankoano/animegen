5057c401b90950e9e43b27186b9f907fbf977d38
Salif Tankoano <tanksalif@gmail.com>
2025-01-01 00:57:06 -0500

    adjust theme switch hover color

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -21,7 +21,7 @@ export function ThemeSwitch() {
         <Button
           variant="ghost"
           size="icon"
-          className="text-primary border-none hover:text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-0"
+          className="text-primary border-none hover:text-black hover:bg-white focus:outline-none focus-visible:ring-0"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
