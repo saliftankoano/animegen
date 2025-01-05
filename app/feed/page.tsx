5b24f067caf1fa48f6de04e2373fb17851eca2da
Salif Tankoano <tanksalif@gmail.com>
2025-01-05 18:35:21 -0500

    complete infinite scroll effect

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ImageCard } from "@/components/ImageCard";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -7,6 +7,7 @@ import { RealtimeChannel } from "@supabase/supabase-js";
 import { useUser } from "@clerk/clerk-react";
 import Image from "next/image";
 import { CreateImageCTA } from "@/components/create-image-cta";
+import { useInView } from "react-intersection-observer";
 
 export interface GeneratedImages {
   id: string;
@@ -24,12 +25,7 @@ export default function Home() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const username = user?.username;
   const [imageFeed, setImageFeed] = useState<GeneratedImages[]>([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const imagesPerPage = 12;
-  const lastImageIndex = currentPage * imagesPerPage;
-  const firstImageIndex = lastImageIndex - imagesPerPage;
-  const currentImages = imageFeed.slice(firstImageIndex, lastImageIndex);
+  const IMAGES_PER_FETCH = 6;
   const [supabaseClient] = useState(() =>
     createClientComponentClient({
       supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -40,9 +36,13 @@ export default function Home() {
     url: string;
     prompt: string;
   } | null>(null);
-  const lastImageRef = useRef(null);
-
-  const [lastImageInView, setLastImageInView] = useState(false);
+  const { ref, inView } = useInView({
+    threshold: 0.4,
+    delay: 100,
+    triggerOnce: true,
+  });
+  const observerRef = useRef<HTMLDivElement | null>(null);
+  const [hasMore, setHasMore] = useState(true);
   // useEffect(() => {
   //   // Initial fetch of images
   //   const fetchUserImages = async () => {
@@ -89,39 +89,84 @@ export default function Home() {
   //     subscription?.unsubscribe();
   //   };
   // }, [supabaseClient, username]);
+  // useEffect(() => {
+  //   const fetchUserImages = async () => {
+  //     const { data, error } = await supabaseClient
+  //       .from("image")
+  //       .select("*")
+  //       .eq("on_feed", true)
+  //       .range(0, 5)
+  //       .order("created_at", { ascending: false });
+
+  //     if (error) {
+  //       console.error("Error fetching images:", error);
+  //       return;
+  //     }
+  //     // Ensure the data is serializable by converting it to plain objects
+  //     const serializedData = JSON.parse(JSON.stringify(data || []));
+  //     setImageFeed(serializedData);
+  //   };
+  //   fetchUserImages();
+  // }, [supabaseClient]);
+
+  // Fetch images from the database
   useEffect(() => {
-    const fetchUserImages = async () => {
+    const fetchUserImages = async (start: number, end: number) => {
       const { data, error } = await supabaseClient
         .from("image")
         .select("*")
         .eq("on_feed", true)
-        .range(0, 5)
+        .range(start, end)
         .order("created_at", { ascending: false });
 
       if (error) {
         console.error("Error fetching images:", error);
         return;
       }
+      const { count } = await supabaseClient
+        .from("image")
+        .select("*", { count: "exact", head: true })
+        .eq("on_feed", true)
+        .lt("created_at", data?.[data.length - 1]?.created_at || "");
+
+      if (count === 0) {
+        setHasMore(false);
+      }
       // Ensure the data is serializable by converting it to plain objects
       const serializedData = JSON.parse(JSON.stringify(data || []));
-      setImageFeed(serializedData);
+      if (imageFeed.length == 0) {
+        setImageFeed(serializedData);
+      } else {
+        setImageFeed((prevImages) => [...prevImages, ...serializedData]);
+      }
     };
-    fetchUserImages();
-  }, [supabaseClient]);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setLastImageInView(entry.isIntersecting);
-        console.log(
-          "Last Image is now in view! Current length = " + imageFeed.length
-        );
-      },
-      { threshold: 1 }
-    );
-    if (lastImageRef.current) {
-      observer.observe(lastImageRef.current);
+    if (imageFeed.length == 0) {
+      fetchUserImages(0, IMAGES_PER_FETCH - 1);
+    } else if (inView && hasMore) {
+      console.log(
+        "Last Image is now in view! Current length = " + imageFeed.length
+      );
+      const nextStartIndex = imageFeed.length;
+      fetchUserImages(nextStartIndex, nextStartIndex + IMAGES_PER_FETCH - 1);
     }
-  }, [lastImageInView, imageFeed.length]);
+  }, [inView, imageFeed.length, supabaseClient, hasMore]);
+
+  // Check if the observer ref or imageFeed length changed and update the observer ref
+  useEffect(() => {
+    if (observerRef.current) {
+      observerRef.current = null;
+    }
+    if (imageFeed.length > 0) {
+      observerRef.current = document.querySelector(
+        `[data-id="${imageFeed.length - 1}"]`
+      );
+    }
+    if (observerRef.current) {
+      ref(observerRef.current);
+      console.log("Observer ref is " + observerRef.current);
+    }
+  }, [imageFeed.length, ref]);
+
   const handleImageClick = (url: string, prompt: string) => {
     setSelectedImage({ url, prompt });
   };
@@ -159,14 +204,12 @@ export default function Home() {
       )}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {currentImages.map((image, index) => (
+        {imageFeed.map((image, index) => (
           <div
             key={image.id}
-            ref={index === currentImages.length - 1 ? lastImageRef : null}
+            data-id={index}
+            ref={index === imageFeed.length - 1 ? observerRef : null}
           >
-            {lastImageInView && index == currentImages.length - 1
-              ? "Yup this one is the last!"
-              : ""}
             <ImageCard
               profile_url={image.profile_url || ""}
               url={image.url || ""}
