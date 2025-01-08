0b9a311a028632633b80edffd2da7f6f37834962
Salif Tankoano <tanksalif@gmail.com>
2025-01-07 22:03:49 -0500

    add service role key to allow upsert on supabase

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
   if (!SIGNING_SECRET) {
     throw new Error(
